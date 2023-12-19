a669ffb927a29e3197ec6040d8f28042b2fea22b
Flor Pons Toloza <111473222+FlorPons@users.noreply.github.com>
2023-12-18 22:34:31 -0300

    Se adhieren p[aginas nuevas

--- RedirectionLogin.js
+++
@@ -1,8 +0,0 @@
-// After completing the registration successfully...
-// Redirects the user to the login page
-app.post('/signup', (req, res) => {
-    // Processes the record and saves the data to the database
-    // ...
-    // It then redirects the user to the login page
-    res.redirect('/login');
-});
